a4a8c4dd6e2ddef8bbd8ab28c55bde1610915a4f
audm00n <auhmoon@ucsc.edu>
2025-05-23 09:51:36 -0700

    Update clustering

--- Project3/clustering.R
+++ Project3/clustering.R
@@ -1,28 +1,37 @@
 # Clustering- FCPS
 install.packages("FCPS")
 library(FCPS)
-library(tidyverse)
-library(cluster)
+library(tidyverse) # data manipulation
+library(cluster) # clustering algorithms
 install.packages("factoextra")
-library(factoextra)
+library(factoextra) # clustering algorithms & visualization
 
 # read data
-pizza_new <- read_csv("Project1/data/pizza_processed.csv")
+pizza_new <- read.csv("Project1/data/pizza_processed.csv")
 
 # create data frame
 df <- data.frame(pizza_new$price_level, pizza_new$review_stats_community_average_score)
-df <- na.omit(df)
+df <- na.omit(df) # removes missing values
 df <- scale(df)
 head(df)
 
 # distance
-distance <- get_dist(df)
+distance <- get_dist(df) # euclidean distance
 fviz_dist(distance, gradient = list(low = "#00AFBB", mid = "white", high = "#FC4E07"))
 
 # apply k-means
 k2 <- kmeans(df, centers = 2, nstart = 25)
 str(k2)
 fviz_cluster(k2, data = df)
+
+# different initial cluster amount
+k2 <- kmeans(df, centers = 4, nstart = 25)
+str(k2)
+fviz_cluster(k2, data = df)
+
+# determining optimal clusters
+set.seed(123)
+fviz_nbclust(df, kmeans, method = "wss")
 
 # # analysis
 # Data = pizza_new$price_level
